9be31d29b74c49e264cad6897380006b0fa51999
bachtv-1072 <truong.viet.bach@sun-asterisk.com>
2020-12-25 09:25:37 +0700

    Task 32852: Build view basic for website

---
+++ app/controllers/admin/adminpages_controller.rb
@@ -0,0 +1,3 @@
+class Admin::AdminpagesController < Admin::BaseController
+  def home; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    namespace :admin do
+      root "adminpages#home"
+    end
+    root "homepages#home"
+  end
 end
